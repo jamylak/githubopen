554c20655f0f62d184b1cfa1c269505ae4e2dd8d
jamylak <jamylak@gmail.com>
2025-03-01 21:14:48 +1100

    get from content script to background

--- ext/background.js
+++ ext/background.js
@@ -1,22 +1,24 @@
-function sendUrlToServer(tab) {
-  if (tab.url) {
-    fetch("http://localhost:5555/receive-url", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ url: tab.url })
-    })
-    .then(response => console.log("URL sent successfully"))
-    .catch(error => console.error("Error sending URL:", error));
-  }
+function sendDataToServer(data) {
+  console.log("Sending data: ", data);
+  fetch("http://localhost:5555/receive-url", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  })
+  .then(response => console.log("Data sent successfully"))
+  .catch(error => console.error("Error sending data:", error));
 }
 
 chrome.commands.onCommand.addListener((command) => {
+  console.log("Get from tab");
   if (command === "send_url") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0) {
-        sendUrlToServer(tabs[0]);
+        chrome.tabs.sendMessage(tabs[0].id, { action: "extract_data" }, (response) => {
+          if (response) {
+            sendDataToServer(response);
+          }
+        });
       }
     });
   }
