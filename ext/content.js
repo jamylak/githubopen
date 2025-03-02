e0edd2b5e7f5114e780834420061f508b11f4136
jamylak <jamylak@gmail.com>
2025-03-02 17:14:29 +1100

    fallback command to get the branch

--- ext/content.js
+++ ext/content.js
@@ -6,6 +6,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const branchElement = document.querySelector(".head-ref");
     if (branchElement)
       pageData.branch = branchElement.textContent;
+    else {
+      // For the commits page on PR eg.
+      // https://github.com/ghostty-org/ghostty/pull/5681/commits
+      // it doesn't use .head-ref
+      const fromCommitAnchor = Array.from(document.querySelectorAll("span")).find(span => span.textContent == "from ");
+      if (fromCommitAnchor)
+        pageData.branch = fromCommitAnchor.nextElementSibling.textContent;
+    }
+
     sendResponse(pageData);
   }
 });
